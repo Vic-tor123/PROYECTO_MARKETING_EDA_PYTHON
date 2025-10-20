495542df47a21394f2e9132f61f717f9af328960
Vic-tor123 <victorcarballido@gmail.com>
2025-10-20 23:37:37 +0100

    imputacion de nulos y calculo de outliers

--- SRC/sp_nulos.py
+++ SRC/sp_nulos.py
@@ -44,3 +44,51 @@ def subplot_col_cat(df):
     #Ajustar disenho
     plt.tight_layout()
     plt.show()
+
+
+
+
+
+        # Columnas numericas, visualizacion de outliers, SUBPLOTS
+
+def subplot_col_num(df):
+
+    col_nums=df.select_dtypes(include='number').columns
+    num_graph = len(col_nums)
+
+    num_rows = (num_graph+2)//2
+
+    fig, axes = plt.subplots(num_graph,2, figsize = (15, num_rows*5))
+
+    for i, col in enumerate(col_nums):
+
+        sns.histplot(data = df, x=col, ax=axes[i,0], bins=200)
+        axes[i,0].set_title(f'Distribucion de {col}')
+        axes[i,0].set_xlabel(col)
+        axes[i,0].set_ylabel('Frecuencia')
+        
+        sns.boxplot(data=df, x=col, ax = axes[i,1])
+        axes[i,1].set_title(f'Boxplot de {col}')
+
+    for j in range(i +1, len (axes)):
+        fig.delaxes(axes[j])
+
+    #Ajustar disenho
+    plt.tight_layout()
+    plt.show()
+
+
+    # calculo numerico de los outliers
+
+def calculo_outliers (df, cols):
+    for col in cols:
+        q_75 = df[col].quantile(0.75)
+        q_25 = df[col].quantile(0.25)
+        rango_itq = q_75 - q_25
+        inferior = q_25 - (rango_itq*1.5)
+        superior = q_75 + (rango_itq*1.5) 
+        outliers = df[(df[col]<inferior) | (df[col]>superior)]
+        num_outliers = len(outliers)
+
+        per_outliers = num_outliers/df.shape[0]*100
+        print(f'En la columna {col.upper()} tenenemos un total de {num_outliers} outliers, lo que representa un {per_outliers}% del total') 
