b0f1549cb63244ad396db314109325ca4fc2e90f
Vic-tor123 <victorcarballido@gmail.com>
2025-10-21 22:25:52 +0100

    Relleno de nulos en numericas

--- SRC/sp_nulos.py
+++ SRC/sp_nulos.py
@@ -2,6 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+from sklearn.experimental import enable_iterative_imputer #instalar antes de importar ver en google pypi sklearn
+from sklearn.impute import KNNImputer, IterativeImputer
 
 
 
@@ -92,3 +94,26 @@ def calculo_outliers (df, cols):
 
         per_outliers = num_outliers/df.shape[0]*100
         print(f'En la columna {col.upper()} tenenemos un total de {num_outliers} outliers, lo que representa un {per_outliers}% del total') 
+
+
+
+
+    # Imputacion de nulos con ITERATIVE (estadisticos avanzados)
+
+def imputar_iterative(df, lista_columnas):
+    iter_imputer = IterativeImputer (max_iter=50,random_state=42)
+    data_imputed = iter_imputer.fit_transform(df[lista_columnas])
+    new_col = [col +"_iterative" for col in lista_columnas]
+    df[new_col]= data_imputed
+    display(df[new_col].describe().T)
+    return df      
+
+
+    # Imputacion de nulos con knn (estadisticos avanzados)
+
+def imputar_knn(df, lista_columnas):
+    knn_imputer = KNNImputer (n_neighbors=5)
+    data_imputed = knn_imputer.fit_transform(df[lista_columnas])
+    new_col = [col +"_knn" for col in lista_columnas]
+    df[new_col]= data_imputed
+    return df
